b71ff6c8db61af51d73d9bd6be3e69702e2dc487
Anushka Joshi <anushkajoshi00000@gmail.com>
2025-07-09 13:13:02 +0530

    Update index.ts

--- utils/hooks/index.ts
+++ utils/hooks/index.ts
@@ -1,6 +1,6 @@
 import { useForm } from "../hooks/useForm";
 import { useFilteredSkills } from "../hooks/useFilteredSkills";
 import { useScroll } from "../hooks/useScroll";
-import { useTheme } from "../hooks/useTheme";
 
-export { useForm, useFilteredSkills, useScroll, useTheme };
+
+export { useForm, useFilteredSkills, useScroll };
